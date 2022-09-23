3c5ea25487e025ea1eeb3967a5e785b4f9dd38c3
TechWriter1984 <oopswow@126.com>
2022-09-23 18:16:14 +0800

    first commit

--- eye.py
+++ eye.py
@@ -1,1 +1,10 @@
+# import necessary libraries
+from datetime import datetime
+from RPi.GPIO import GPIO
+from time import sleep
+from pygame import camera
+from pygame import image
 
+now = datetime.now()
+now = now.strftime("%Y%m%d%H%M%S")
+# print(now)
