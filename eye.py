cf34d6020f64975fcf9763a97650c3412888921b
MLGBXXL <oopswow@126.com>
2022-10-20 19:37:10 +0800

    update eye.py based on the AIY Maker Project

--- eye.py
+++ eye.py
@@ -1,10 +1,16 @@
 # import necessary libraries
+import os.path
 from datetime import datetime
-from RPi.GPIO import GPIO
 from time import sleep
-from pygame import camera
-from pygame import image
 
-now = datetime.now()
-now = now.strftime("%Y%m%d%H%M%S")
-# print(now)
+# from pygame import camera
+# from pygame import image
+
+from picamera import PiCamera
+from RPi.GPIO import GPIO
+from aiymakerkit import vision
+
+timestamp = datetime.now()
+filename = "LaoTie_" + timestamp.strftime("%Y%m%d%H%M%S") + '.png'
+filename = os.path.join(PICTURE_DIR, filename)
+vision.save_frame(filename, frame)
