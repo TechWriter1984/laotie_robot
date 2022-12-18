ec554130a52f287a518468dfe121302143b872db
TechWriter1984 <oopswow@126.com>
2022-12-18 16:17:06 +0800

    change design fully

--- conversation.py
+++ conversation.py
@@ -1,76 +1,30 @@
-from aip import AipSpeech
-from pyaudio import PyAudio,paInt16
-import requests,json,wave,time,pyttsx3
-#百度AI，配置信息
-APP_ID = ''#百度ai应用id
-API_KEY = ''#百度ai应用的键值
-SECRET_KEY = ''
-client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
-#图灵机器，配置信息
-TulingUrl='http://openapi.tuling123.com/openapi/api/v2'
-turing_api_key = ""#图灵机器人api_key
- 
-# 读取文件
-def get_file_content(filePath):
-    with open(filePath, 'rb') as fp:
-        return fp.read()
- 
-#用户语音输入
-def SaveVoice():
-    pa=PyAudio()
-    wf=wave.open(r'T.wav','wb')#打开wave文件
-    wf.setnchannels(1)#配置声道数
-    wf.setsampwidth(2)# 采样宽度2bytes
-    wf.setframerate(16000)#采样率
-    stream=pa.open(format=paInt16,channels=wf.getnchannels(),rate=wf.getframerate(),input=True,frames_per_buffer=1024)  #打开一个stream
-    buff=[]#存储声音信息
-    start=time.time()#开始运行时间戳
-    print('用户说：')
-    while time.time()<start+6:#录制6秒
-        buff.append(stream.read(wf.getframerate()))
-    stream.close()#关闭stream
-    pa.terminate()
-    wf.writeframes(b''.join(buff))
-    wf.close()#关闭wave
- 
-#接受机器人响应
-def RobotSpeakText(usersay="你好"):
-    robot={
-        "perception": {
-            "inputText": {
-                "text": usersay
-            }
-        },
-        "userInfo": {
-            "apiKey": turing_api_key ,
-            "userId": 'Dudu'
-         }
-    }
-    response=json.loads(requests.post(TulingUrl,None,robot,headers={'Content-Type':'charset=UTF-8'}).text)
-    return str(response['results'][0]['values']['text'])
- 
-#语音发声
-def RobotVoice(robotsay="我没听清，你在说一遍"):
-    engine=pyttsx3.init()
-    rate=engine.getProperty('rate')
-    engine.setProperty('rate',rate-66)
-    engine.say(robotsay)
+
+
+import speech_recognition as sr
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import pyttsx3
+
+# 将麦克风识别到的.wav语音文件转成.txt文本文件
+def speech_to_text():
+    pass
+
+# 基于chatterbot对.txt文本文件中的内容给出回复，转存成.txt文件
+def smart_chatter():
+    pass
+
+# 基于pyttsx3，将.txt文本文件中的回复，转成.wav文件，并使用自定义的声音读出来
+# 主要逻辑使用if else结构
+def text2speech(goalStr):
+    engine = pyttsx3.init()
+    engine.setProperty('voice', "com.apple.speech.synthesis.voice.sin-ji")  # 需要重新设置成自己想要的语音包
+
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate', rate-40)
+    engine.setProperty('volume', 0.7)
+    engine.say(goalStr)
     engine.runAndWait()
- 
-#主函数
-def main():
-    while True:
-        try:
-            SaveVoice()
-            result = client.asr(get_file_content('T.wav'), 'wav', 16000, {'dev_pid': 1536})  # 识别本地文件
-            usersay=result['result'][0]
-            print(usersay)
-            robotsay=RobotSpeakText(usersay)
-            print('小嘟嘟说：')
-            print(robotsay)
-            RobotVoice(robotsay)
-        except:#异常处理
-            break
- 
-if __name__=='__main__':
-    main()
+
+if __name__ == '__main':
+    strValue = "这是一个测试！"
+    speak(strValue)
