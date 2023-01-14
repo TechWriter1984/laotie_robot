2b72deed9539b04d2fcb8b1e231d381925d02ee4
TechWriter1984 <oopswow@126.com>
2023-01-14 19:02:37 +0800

    fix runtime error

--- pygame_cam_test.py
+++ pygame_cam_test.py
@@ -5,17 +5,15 @@ import pygame.camera
 pygame.init()
 pygame.camera.init()
 
-SCREEN_SIZE = (640, 480)
-CAMERA_SIZE = (400, 300)
-
-screen = pygame.display.set_mode(SCREEN_SIZE, 0)
+screen = pygame.display.set_mode((800, 600), 0)
+pygame.display.set_caption("老铁机器人")
 cam_list = pygame.camera.list_cameras()
-cam = pygame.camera.Camera(cam_list[0], CAMERA_SIZE)
+cam = pygame.camera.Camera(cam_list[0], (640, 480))
 cam.start()
 
 while True:
     image1 = cam.get_image()
-    image1 = pygame.transform.scale(image1, SCREEN_SIZE)
+    image1 = pygame.transform.scale(image1, (800, 600))
     screen.blit(image1,(0,0))
     pygame.display.update()
 
