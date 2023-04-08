cc01a478fafd1afbfabe9c35fd95e6784b44550a
Dan <oopswow@126.com>
2023-04-08 16:00:55 +0800

    fix pygame video system not initialized error

--- camera.py
+++ camera.py
@@ -6,20 +6,25 @@ from datetime import datetime
 import os
 import sys
 
+pygame.init()
+pygame.camera.init()
+pygame.display.set_caption("老铁机器人")
+pygame.display.update()
+
 class CameraEye():
 
     def __init__(self):
-        pygame.init()
-        pygame.camera.init()
+        # pygame.init()
+        # pygame.camera.init()
         self.screen = pygame.display.set_mode((800, 600), 0)
-        pygame.display.set_caption("老铁机器人")
+        # pygame.display.set_caption("老铁机器人")
         self.cam_list = pygame.camera.list_cameras()
         self.cam = pygame.camera.Camera(cam_list[0], (640, 480))
         self.cam.start()
         self.image1 = cam.get_image()
         self.image1 = pygame.transform.scale(image1, (800, 600))
         self.screen.blit(image1,(0,0))
-        pygame.display.update()
+        # pygame.display.update()
 
 
     def capture_photo(self):
