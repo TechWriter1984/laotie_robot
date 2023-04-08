2bf890b4f0caf023bd355327c3cfc1ffb4e73fd6
Dan <oopswow@126.com>
2023-04-08 16:07:06 +0800

    fix the NameError: name 'cam_list' is not defined

--- camera.py
+++ camera.py
@@ -19,11 +19,11 @@ class CameraEye():
         self.screen = pygame.display.set_mode((800, 600), 0)
         # pygame.display.set_caption("老铁机器人")
         self.cam_list = pygame.camera.list_cameras()
-        self.cam = pygame.camera.Camera(cam_list[0], (640, 480))
+        self.cam = pygame.camera.Camera(self.cam_list[0], (640, 480))
         self.cam.start()
-        self.image1 = cam.get_image()
-        self.image1 = pygame.transform.scale(image1, (800, 600))
-        self.screen.blit(image1,(0,0))
+        self.image1 = self.cam.get_image()
+        self.image1 = pygame.transform.scale(self.image1, (800, 600))
+        self.screen.blit(self.image1,(0,0))
         pygame.display.update()
 
 
